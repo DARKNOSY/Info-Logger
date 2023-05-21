1016966a06e40a3cafb9b536ae4cf2b843cee2ce
DARKNOSY <109553205+DARKNOSY@users.noreply.github.com>
2023-05-21 19:49:14 +0200

    Add files via upload

---
+++ Info-Logger/Info-Logger.bat
@@ -0,0 +1,20 @@
+:: Coded by @DARKNOSY (https://github.com/DARKNOSY/Info-Logger)
+@echo off & cls
+color 5
+
+set question-asked=Your Webhook
+:: put question instead of "Your Webhook"
+set /p webhook="%question-asked%:  "
+::if you want to set your own webhook and make it so it doesn't ask for the webhook replace "set /p webhook="WebHook:  "" by "set webhook=WEBHOOK-HERE" and replace "WEBHOOK-HERE" by your webhook link
+set /p message="Message:  " 
+:: the text entered is then sent to the set webhook link (you can replace the question)
+set avatar_url=https://avatars.githubusercontent.com/u/109553205?v=4 
+:: webhook avatar url
+set name=Info Logger / @DARKNOSY
+:: webhook name
+
+curl -H "Content-Type: application/json" -d "{\"username\": \"%name%\", \"content\":\"%question-asked%:  %message%\", \"avatar_url\":\"%avatar_url%\"}" %webhook% >nul & echo Successfuly sent!
+:: send post request to the set webhook
+echo Don't forget to check out the code, it contains all the information you need
+pause
+exit
